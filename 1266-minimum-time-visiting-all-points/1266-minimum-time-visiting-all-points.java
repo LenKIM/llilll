99ea81cab348112c942287977f88843990e91538
simplify-len <joenggyu0@trevari.co.kr>
2022-09-22 09:48:35 +0900

    Time: 1 ms (97.04%), Space: 44.7 MB (7.23%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.java
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.java
@@ -1,5 +1,7 @@
 /**
-11:40
+1시간 투자했는데, 못풀었다가 잠시 셨다가 다시 풀었더니 풀렸다.
+억울하게도, 단지 거리만 구하는 쉬운 문제였음에도 어렵게 생각했다.
+
 **/
 class Solution {
     public int minTimeToVisitAllPoints(int[][] points) {
@@ -14,16 +16,6 @@ class Solution {
             
             int xEnd = end[0];
             int yEnd = end[1];
-            
-//             if(xBegin == xEnd) {
-//                 result = result + Math.abs(yBegin-yEnd);
-//                 continue;
-//             }
-            
-//             if(yBegin == yEnd) {
-//                 result = result + Math.abs(xBegin-xEnd);
-//                 continue;
-//             }
             
             int a = Math.abs(xBegin - xEnd);
             int b = Math.abs(yBegin - yEnd);
